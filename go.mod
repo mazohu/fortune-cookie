7c64a6351d0626482428b66eda07979d7c3425ef
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2023-02-04 13:49:38 -0500

    Authentication using Bun Router
    https://github.com/uptrace/bunrouter/blob/master/example/basicauth/main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,24 @@
 module sample-app/main.go
 
 go 1.20
+
+require (
+	github.com/0xAX/notificator v0.0.0-20220220101646-ee9b8921e557 // indirect
+	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
+	github.com/codegangsta/gin v0.0.0-20211113050330-71f90109db02 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-shellwords v1.0.12 // indirect
+	github.com/rs/cors v1.8.3 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/uptrace/bunrouter v1.0.19 // indirect
+	github.com/uptrace/bunrouter/extra/basicauth v1.0.19 // indirect
+	github.com/uptrace/bunrouter/extra/reqlog v1.0.19 // indirect
+	github.com/urfave/cli v1.22.12 // indirect
+	go.opentelemetry.io/otel v1.9.0 // indirect
+	go.opentelemetry.io/otel/trace v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+)
--- sample-app/main.go
+++ sample-app/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"crypto/subtle"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/uptrace/bunrouter"
+	"github.com/uptrace/bunrouter/extra/basicauth"
+	"github.com/uptrace/bunrouter/extra/reqlog"
+)
 
 func main() {
-	fmt.Println("aaaaaaaa")
+	router := bunrouter.New(
+		bunrouter.Use(reqlog.NewMiddleware(
+			reqlog.FromEnv("BUNDEBUG"),
+		)),
+	)
+
+	authMiddleware := basicauth.NewMiddleware(authUser, basicauth.WithRealm("test:test"))
+	router.GET("/", indexHandler)
+
+	router.Use(authMiddleware).
+		WithGroup("/restricted", func(g *bunrouter.Group) {
+			g.GET("", debugHandler)
+		})
+
+	router.WithGroup("/public", func(g *bunrouter.Group) {
+		g.GET("", debugHandler)
+	})
+
+	log.Println("listening on http://localhost:9999")
+	log.Println(http.ListenAndServe(":9999", router))
+}
+
+func indexHandler(w http.ResponseWriter, req bunrouter.Request) error {
+	return indexTemplate().Execute(w, nil)
+}
+
+func debugHandler(w http.ResponseWriter, req bunrouter.Request) error {
+	return bunrouter.JSON(w, bunrouter.H{
+		"route":  req.Route(),
+		"params": req.Params().Map(),
+	})
+}
+
+func authUser(req bunrouter.Request) (bool, error) {
+	user, pass, ok := req.BasicAuth()
+	if !ok {
+		return false, nil
+	}
+
+	if subtle.ConstantTimeCompare([]byte(user), []byte("test")) == 1 &&
+		subtle.ConstantTimeCompare([]byte(pass), []byte("test")) == 1 {
+		return true, nil
+	}
+	return false, nil
+}
+
+var indexTmpl = `
+<html>
+  <h1>Welcome</h1>
+  <ul>
+    <li><a href="/restricted">/restricted</a></li>
+    <li><a href="/public">/public</a></li>
+  </ul>
+</html>
+`
+
+func indexTemplate() *template.Template {
+	return template.Must(template.New("index").Parse(indexTmpl))
 }
