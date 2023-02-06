88c4bd29fb2e6714825e6b745d50808f151020e0
bencrbcp <bencrbcp@gmail.com>
2023-02-05 19:27:56 -0500

    redirected the routing for the '/' path to point to the landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -46,7 +46,7 @@ const routes: Routes = [
   // no layout views
   { path: "profile", component: ProfileComponent },
   { path: "landing", component: LandingComponent },
-  { path: "", component: IndexComponent },
+  { path: "", component: LandingComponent },
   { path: "**", redirectTo: "", pathMatch: "full" },
 ];
 
