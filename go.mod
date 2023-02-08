04bc155b69b2370a60e3ee7cea13bfbddfc998f6
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2023-02-08 16:59:03 -0500

    With Environments
    Working

--- go.mod
+++ go.mod
@@ -9,14 +9,29 @@ require (
 )
 
 require (
-	cloud.google.com/go v0.67.0 // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
+	cloud.google.com/go v0.105.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
-	google.golang.org/appengine v1.6.6 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/spf13/afero v1.9.3 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.15.0 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20221014153046-6fdb5e3db783 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -16,122 +16,65 @@
 package main
 
 import (
+  "fmt"
+  "html/template"
+  "net/http"
+  
 
-	"fmt"
-	"os"
-	"log"
-  	"github.com/joho/godotenv"
+  "log"
+
+  "github.com/gorilla/pat"
+  "github.com/markbates/goth"
+  "github.com/markbates/goth/gothic"
+  "github.com/markbates/goth/providers/google"
+  "github.com/gorilla/sessions"
+
+	"github.com/spf13/viper"
 )
 
 
-// use godot package to load/read the .env file and
-// return the value of the key
-func goDotEnvVariable(key string) string {
-
-  // load .env file
-  err := godotenv.Load(".env")
-
-  if err != nil {
-    log.Fatalf("Error loading .env file")
-  }
-
-  return os.Getenv(key)
-}
-
-func envVariable(key string) string {
-
-	// set env variable using os package
-	os.Setenv(key, "gopher")
-  
-	// return the env variable using os package
-	return os.Getenv(key)
-  }
-
 func main() {
 
-	// os package
-	value := envVariable("name")
-  
-	fmt.Printf("os package: name = %s \n", value)
-	fmt.Printf("environment = %s \n", os.Getenv("APP_ENV"))
+	viper.SetConfigFile(".env")
+    viper.ReadInConfig()
 
+	key := "session_secret"  // Replace with your SESSION_SECRET or similar
+	maxAge := 86400 * 30  // 30 days
+	isProd := false       // Set to true when serving over https
 
-  // godotenv package
-  dotenv := goDotEnvVariable("STRONGEST_AVENGER")
+	store := sessions.NewCookieStore([]byte(key))
+	store.MaxAge(maxAge)
+	store.Options.Path = "/"
+	store.Options.HttpOnly = true   // HttpOnly should always be enabled
+	store.Options.Secure = isProd
 
-  fmt.Printf("godotenv : %s = %s \n", "STRONGEST_AVENGER", dotenv)
+	gothic.Store = store
+
+	goth.UseProviders(
+		//google.New("our-google-client-id", "our-google-client-secret", "http://localhost:3000/auth/google/callback", "email", "profile"),
+		google.New(viper.GetString("GOOGLE_CLIENT_ID"), viper.GetString("GOOGLE_CLIENT_SECRET"), "http://localhost:3000/auth/google/callback", "email", "profile"),
+	)
+
+	p := pat.New()
+	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+
+		user, err := gothic.CompleteUserAuth(res, req)
+		if err != nil {
+		fmt.Fprintln(res, err)
+		return
+		}
+		t, _ := template.ParseFiles("templates/success.html")
+		t.Execute(res, user)
+	})
+
+	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+		gothic.BeginAuthHandler(res, req)
+	})
+
+	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		t, _ := template.ParseFiles("templates/index.html")
+		t.Execute(res, false)
+	})
+	log.Println("listening on localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", p))
 }
-
-// package main
-
-// import (
-//   "fmt"
-//   "html/template"
-//   "net/http"
-  
-
-//   "log"
-
-//   "github.com/gorilla/pat"
-//   "github.com/markbates/goth"
-//   "github.com/markbates/goth/gothic"
-//   "github.com/markbates/goth/providers/google"
-//   "github.com/gorilla/sessions"
-//   "github.com/joho/godotenv"
-//   "os"
-// )
-
-// func goDotEnvVariable(key string) string {
-
-// 	// load .env file
-// 	err := godotenv.Load(".env")
-  
-// 	if err != nil {
-// 	  log.Fatalf("Error loading .env file")
-// 	}
-  
-// 	return os.Getenv(key)
-//   }
-
-
-// func main() {
-  
-//   key := goDotEnvVariable("SECRET-SESSION-KEY")  // Replace with your SESSION_SECRET or similar
-//   maxAge := 86400 * 30  // 30 days
-//   isProd := false       // Set to true when serving over https
-
-//   store := sessions.NewCookieStore([]byte(key))
-//   store.MaxAge(maxAge)
-//   store.Options.Path = "/"
-//   store.Options.HttpOnly = true   // HttpOnly should always be enabled
-//   store.Options.Secure = isProd
-
-//   gothic.Store = store
-
-//   goth.UseProviders(
-//     google.New("our-google-client-id", "our-google-client-secret", "http://localhost:3000/auth/google/callback", "email", "profile"),
-//   )
-
-//   p := pat.New()
-//   p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
-
-//     user, err := gothic.CompleteUserAuth(res, req)
-//     if err != nil {
-//       fmt.Fprintln(res, err)
-//       return
-//     }
-//     t, _ := template.ParseFiles("templates/success.html")
-//     t.Execute(res, user)
-//   })
-
-//   p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
-//     gothic.BeginAuthHandler(res, req)
-//   })
-
-//   p.Get("/", func(res http.ResponseWriter, req *http.Request) {
-//     t, _ := template.ParseFiles("templates/index.html")
-//     t.Execute(res, false)
-//   })
-//   log.Println("listening on localhost:3000")
-//   log.Fatal(http.ListenAndServe(":3000", p))
-// }
