622c84f68502db11dc3b1ed9678b0182055a6b43
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2023-02-08 16:27:04 -0500

    Trying to use .env

--- go.mod
+++ go.mod
@@ -3,14 +3,18 @@ module ourProject
 go 1.20
 
 require (
+	github.com/gorilla/pat v1.0.1
+	github.com/gorilla/sessions v1.2.1
+	github.com/markbates/goth v1.76.0
+)
+
+require (
 	cloud.google.com/go v0.67.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/gorilla/pat v1.0.1
 	github.com/gorilla/securecookie v1.1.1 // indirect
-	github.com/gorilla/sessions v1.2.1
-	github.com/markbates/goth v1.76.0
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
--- main.go
+++ main.go
@@ -16,59 +16,122 @@
 package main
 
 import (
-  "fmt"
-  "html/template"
-  "net/http"
-  
 
-  "log"
-
-  "github.com/gorilla/pat"
-  "github.com/markbates/goth"
-  "github.com/markbates/goth/gothic"
-  "github.com/markbates/goth/providers/google"
-  "github.com/gorilla/sessions"
+	"fmt"
+	"os"
+	"log"
+  	"github.com/joho/godotenv"
 )
 
 
-func main() {
-  
-  key := "Secret-session-key"  // Replace with your SESSION_SECRET or similar
-  maxAge := 86400 * 30  // 30 days
-  isProd := false       // Set to true when serving over https
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
 
-  store := sessions.NewCookieStore([]byte(key))
-  store.MaxAge(maxAge)
-  store.Options.Path = "/"
-  store.Options.HttpOnly = true   // HttpOnly should always be enabled
-  store.Options.Secure = isProd
+  // load .env file
+  err := godotenv.Load(".env")
 
-  gothic.Store = store
+  if err != nil {
+    log.Fatalf("Error loading .env file")
+  }
 
-  goth.UseProviders(
-    google.New("our-google-client-id", "our-google-client-secret", "http://localhost:3000/auth/google/callback", "email", "profile"),
-  )
-
-  p := pat.New()
-  p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
-
-    user, err := gothic.CompleteUserAuth(res, req)
-    if err != nil {
-      fmt.Fprintln(res, err)
-      return
-    }
-    t, _ := template.ParseFiles("templates/success.html")
-    t.Execute(res, user)
-  })
-
-  p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
-    gothic.BeginAuthHandler(res, req)
-  })
-
-  p.Get("/", func(res http.ResponseWriter, req *http.Request) {
-    t, _ := template.ParseFiles("templates/index.html")
-    t.Execute(res, false)
-  })
-  log.Println("listening on localhost:3000")
-  log.Fatal(http.ListenAndServe(":3000", p))
+  return os.Getenv(key)
 }
+
+func envVariable(key string) string {
+
+	// set env variable using os package
+	os.Setenv(key, "gopher")
+  
+	// return the env variable using os package
+	return os.Getenv(key)
+  }
+
+func main() {
+
+	// os package
+	value := envVariable("name")
+  
+	fmt.Printf("os package: name = %s \n", value)
+	fmt.Printf("environment = %s \n", os.Getenv("APP_ENV"))
+
+
+  // godotenv package
+  dotenv := goDotEnvVariable("STRONGEST_AVENGER")
+
+  fmt.Printf("godotenv : %s = %s \n", "STRONGEST_AVENGER", dotenv)
+}
+
+// package main
+
+// import (
+//   "fmt"
+//   "html/template"
+//   "net/http"
+  
+
+//   "log"
+
+//   "github.com/gorilla/pat"
+//   "github.com/markbates/goth"
+//   "github.com/markbates/goth/gothic"
+//   "github.com/markbates/goth/providers/google"
+//   "github.com/gorilla/sessions"
+//   "github.com/joho/godotenv"
+//   "os"
+// )
+
+// func goDotEnvVariable(key string) string {
+
+// 	// load .env file
+// 	err := godotenv.Load(".env")
+  
+// 	if err != nil {
+// 	  log.Fatalf("Error loading .env file")
+// 	}
+  
+// 	return os.Getenv(key)
+//   }
+
+
+// func main() {
+  
+//   key := goDotEnvVariable("SECRET-SESSION-KEY")  // Replace with your SESSION_SECRET or similar
+//   maxAge := 86400 * 30  // 30 days
+//   isProd := false       // Set to true when serving over https
+
+//   store := sessions.NewCookieStore([]byte(key))
+//   store.MaxAge(maxAge)
+//   store.Options.Path = "/"
+//   store.Options.HttpOnly = true   // HttpOnly should always be enabled
+//   store.Options.Secure = isProd
+
+//   gothic.Store = store
+
+//   goth.UseProviders(
+//     google.New("our-google-client-id", "our-google-client-secret", "http://localhost:3000/auth/google/callback", "email", "profile"),
+//   )
+
+//   p := pat.New()
+//   p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+
+//     user, err := gothic.CompleteUserAuth(res, req)
+//     if err != nil {
+//       fmt.Fprintln(res, err)
+//       return
+//     }
+//     t, _ := template.ParseFiles("templates/success.html")
+//     t.Execute(res, user)
+//   })
+
+//   p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+//     gothic.BeginAuthHandler(res, req)
+//   })
+
+//   p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+//     t, _ := template.ParseFiles("templates/index.html")
+//     t.Execute(res, false)
+//   })
+//   log.Println("listening on localhost:3000")
+//   log.Fatal(http.ListenAndServe(":3000", p))
+// }
