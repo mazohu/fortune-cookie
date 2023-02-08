9f9d7a624736c83c08ad84980e6a36482b47fea4
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2023-02-08 16:01:14 -0500

    Another Tutorial Another Attempt

---
+++ go.mod
@@ -0,0 +1,18 @@
+module ourProject
+
+go 1.20
+
+require (
+	cloud.google.com/go v0.67.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/pat v1.0.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	github.com/markbates/goth v1.76.0 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,74 @@
+//* Below is me just trying to get personally made packages to work ignore it
+// package main
+
+// import (
+// 	"fmt"
+// 	//"ourProject/helper"
+// )
+
+// func main() {
+//     fmt.Println("hello world")
+// 	//helper.Help();
+// }
+
+//* Below is the start of the tutorial
+
+package main
+
+import (
+  "fmt"
+  "html/template"
+  "net/http"
+  
+
+  "log"
+
+  "github.com/gorilla/pat"
+  "github.com/markbates/goth"
+  "github.com/markbates/goth/gothic"
+  "github.com/markbates/goth/providers/google"
+  "github.com/gorilla/sessions"
+)
+
+
+func main() {
+  
+  key := "Secret-session-key"  // Replace with your SESSION_SECRET or similar
+  maxAge := 86400 * 30  // 30 days
+  isProd := false       // Set to true when serving over https
+
+  store := sessions.NewCookieStore([]byte(key))
+  store.MaxAge(maxAge)
+  store.Options.Path = "/"
+  store.Options.HttpOnly = true   // HttpOnly should always be enabled
+  store.Options.Secure = isProd
+
+  gothic.Store = store
+
+  goth.UseProviders(
+    google.New("our-google-client-id", "our-google-client-secret", "http://localhost:3000/auth/google/callback", "email", "profile"),
+  )
+
+  p := pat.New()
+  p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
+
+    user, err := gothic.CompleteUserAuth(res, req)
+    if err != nil {
+      fmt.Fprintln(res, err)
+      return
+    }
+    t, _ := template.ParseFiles("templates/success.html")
+    t.Execute(res, user)
+  })
+
+  p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
+    gothic.BeginAuthHandler(res, req)
+  })
+
+  p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+    t, _ := template.ParseFiles("templates/index.html")
+    t.Execute(res, false)
+  })
+  log.Println("listening on localhost:3000")
+  log.Fatal(http.ListenAndServe(":3000", p))
+}
