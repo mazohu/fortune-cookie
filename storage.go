b926eacd5a4d087911f9afbff3cc2ffd19247264
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2023-02-22 22:35:54 -0500

    More Databases!
    Found a way to locate our specific user in the database, and if not, we'd create a new entry for them.

--- storage.go
+++ storage.go
@@ -29,39 +29,48 @@ func createDatabase(user string, em string, uID string) {
 	// Migrate the schema
 	db.AutoMigrate(&People{})
 
-	//human := People{Username: user, Email: em, UserID: uID, Temp: 5}
-	//db.Create(&human)
-	//log.Println(human.ID, " is my database ID")
+	//Initialize struct variable
+	var person People
+
+	//delete the first so its clear
+	//db.Unscoped().Delete(&person, 1)
 
 	//Create
 	//db.Create(&People{Username: user, Email: em, UserID: uID, Temp: 5})
+	ourPerson := People{Username: user, Email: em, UserID: uID, Temp: 5}
+
+	result := db.Find(&person, "user_id = ?", uID)
+	if (result.RowsAffected <= 0){
+		db.Create(&ourPerson)
+		log.Println("Our person wasn't found, so we made a new one")
+	} else{
+		log.Println("Our person found!")
+	}
 	
 	//Read
-	var person People
-  	db.First(&person, 1) // find product with integer primary key
+  	db.First(&person, "user_id = ?", uID) // find product with integer primary key
 	log.Println(person.ID, " is my database ID")
   	//db.First(&person, "Username = ?", "Email = ?", "UserID = ?", "5") // find person with temp = 5
 
 	// Update - update temp to 10
-	db.Model(&person).Update("Temp", 10)
+	//db.Model(&person).Update("Temp", 10)
 
-	// if (uID == person.){
-	// 	//This works!
-	// 	log.Println("True!");
-	// }
+	
 
 	//retrieving a database by finding the ID
-	db.Where("UserID = ?", uID).First(&person)
+	db.Debug().Where("user_id = ?", uID).First(&person)
+	//db.Debug().Select("username").Where("user_id = ?", uID).Table("people")
 
 
 	//Update - update multiple fields
 	//db.Model(&person).Updates(Storage{Username: "200", Email: "F42"}) // non-zero fields
 	//db.Model(&person).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
   
-	// Delete - delete person
-	db.Unscoped().Delete(&person, 2)
-	db.Unscoped().Delete(&person, 3)
-	db.Unscoped().Delete(&person, 4)
-	db.Unscoped().Delete(&person, 5)
-	db.Unscoped().Delete(&person, 6)
+	// Delete - PERMANANTLY delete person (by ID)
+	// db.Unscoped().Delete(&person, 1)
+	// db.Unscoped().Delete(&person, 2)
+	// db.Unscoped().Delete(&person, 3)
+	// db.Unscoped().Delete(&person, 4)
+	// db.Unscoped().Delete(&person, 5)
+	// db.Unscoped().Delete(&person, 6)
 }
