1dff87dc01947db7afa03bea549c8b4469b1129e
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2023-02-22 22:02:20 -0500

    Attempting a database
    A lot of practice with gorm

--- go.mod
+++ go.mod
@@ -18,7 +18,10 @@ require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/spf13/afero v1.9.3 // indirect
@@ -34,4 +37,6 @@ require (
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/sqlite v1.4.4 // indirect
+	gorm.io/gorm v1.24.5 // indirect
 )
--- storage.go
+++ storage.go
@@ -2,11 +2,66 @@ package main
 
 import (
 	"log"
+
+	"gorm.io/gorm"
+  	"gorm.io/driver/sqlite"
 )
 
-func createDatabase(username string, email string, userID string) {
+type People struct {
+	gorm.Model
+	Username  string
+	Email string
+	UserID string
+	Temp int
+  }
+
+func createDatabase(user string, em string, uID string) {
 	log.Println("Authorization successful!")
-	log.Println("Username: ", username)
-	log.Println("Email: ", email)
-	log.Println("UID: ", userID)
+	log.Println("Username: ", user)
+	log.Println("Email: ", em)
+	log.Println("UID: ", uID)
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+	  panic("failed to connect database")
+	}
+  
+	// Migrate the schema
+	db.AutoMigrate(&People{})
+
+	//human := People{Username: user, Email: em, UserID: uID, Temp: 5}
+	//db.Create(&human)
+	//log.Println(human.ID, " is my database ID")
+
+	//Create
+	//db.Create(&People{Username: user, Email: em, UserID: uID, Temp: 5})
+	
+	//Read
+	var person People
+  	db.First(&person, 1) // find product with integer primary key
+	log.Println(person.ID, " is my database ID")
+  	//db.First(&person, "Username = ?", "Email = ?", "UserID = ?", "5") // find person with temp = 5
+
+	// Update - update temp to 10
+	db.Model(&person).Update("Temp", 10)
+
+	// if (uID == person.){
+	// 	//This works!
+	// 	log.Println("True!");
+	// }
+
+	//retrieving a database by finding the ID
+	db.Where("UserID = ?", uID).First(&person)
+
+
+	//Update - update multiple fields
+	//db.Model(&person).Updates(Storage{Username: "200", Email: "F42"}) // non-zero fields
+	//db.Model(&person).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+  
+	// Delete - delete person
+	db.Unscoped().Delete(&person, 2)
+	db.Unscoped().Delete(&person, 3)
+	db.Unscoped().Delete(&person, 4)
+	db.Unscoped().Delete(&person, 5)
+	db.Unscoped().Delete(&person, 6)
 }
