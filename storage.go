8ef4829786989ed215204ef79134dcb9541fd1d8
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2023-02-27 17:32:27 -0500

    Functions and Databases
    - Added new functions pertaining to testing time and submitting fortunes
    - made new database test3 which doesn't try to change the database primary key

--- storage.go
+++ storage.go
@@ -16,11 +16,11 @@
 	just look at the table and not your variables.
 
 & TO DO
-	- Need a timer / date thing to see what to set the bool Submitted to	
 	- Need a way to add to the fortunes database
 	- Need a way to retrieve from the fortunes database 
 		- use random numbers 1 - length of database
 		- Need to make sure they don't receive their own fortunes
+	- Look into making the ids into primary keys
 */
 
 package main
@@ -39,7 +39,7 @@ type Users struct {
 
 	Username  string		//General Info about the User
 	Email string
-	UserID string			`gorm:"primaryKey;autoIncrement:false"`
+	UserID string			//`gorm:"primaryKey;autoIncrement:false"` -> tried this with test2.db
 
 	Fid string				//Stores the Ids of all the fortunes recieved, for history. It's in order.
 	Submitted bool			//If a fortune has been submitted by them today.
@@ -50,85 +50,13 @@ type Users struct {
   type Fortunes struct {
 	gorm.Model
 
-	Fid string				`gorm:"primaryKey;autoIncrement:false"`
+	Fid string				//`gorm:"primaryKey;autoIncrement:false"`
 	Author string
 	Text string
 
   }
 
-func accessDatabase(username string, em string, uID string) {
-	log.Println("Authorization successful!")
-	log.Println("Username: ", username)
-	log.Println("Email: ", em)
-	log.Println("UID: ", uID)
-
-	//opening the test database
-	db, err := gorm.Open(sqlite.Open("test2.db"), &gorm.Config{})
-	if err != nil {
-	  panic("failed to connect database")
-	}
-  
-	// Migrate the schema
-	db.AutoMigrate(&Users{})
-
-	//Initialize struct variable
-	var userPointer Users
-
-	//!For Testing purposes, I'm deleting the first entry each time we restart the program
-	//db.Unscoped().Delete(&userPointer, 1)
-	//db.Unscoped().Delete(&userPointer, 2)
-	
-	//Create
-	ourPerson := Users{Username: username, Email: em, UserID: uID, Fid: "", Submitted: false, LastTime: time.Date(2002, time.January, 1, 23, 0, 0, 0, time.UTC)}
-
-	//Check if we have this user in the database already, or if we need to make a new row
-	//~ I made log outputs for testing purposes, but after that is the commented out version of the same thing but less code
-	result := db.Find(&userPointer, "user_id = ?", uID)
-	if (result.RowsAffected <= 0){
-		db.Create(&ourPerson)
-		log.Println("Our person wasn't found, so we made a new entry to the database")
-	} else{
-		log.Println("Our person found!")
-	}
-
-	/*// ! This is the same thing as above but less code, replace once we're sure things are ok.
-	result := db.Find(&userPointer, "user_id = ?", uID)
-	if (result.RowsAffected <= 0){
-		db.Create(&ourPerson)
-	} 
-	*/
-
-	//Setting the pointer so it can retrieve the userID and also update the database
-	db.First(&userPointer, "user_id = ?", uID).Scan(&userPointer)
-
-	// ~ Testing fortuneTimer
-	var hasChanged bool = false
-	hasChanged = checkTime(userPointer)
-	log.Println("\nHas our time changed? ", hasChanged)
-
-	//Updating the database after any changes
-	db.Model(&userPointer).Update("submitted", userPointer.Submitted)
-	db.Model(&userPointer).Update("fid", userPointer.Fid)
-	db.Model(&userPointer).Update("last_time", userPointer.LastTime)
-
-	log.Println(userPointer.ID, " is my database ID")
-	
-	// ~ For Testing
-	// log.Println("Our temp is ", anotherPerson.Temp)
-	// log.Println(userPointer.UserID, " is my database ID")
-	// log.Println(userPointer.Email, " is my database email")
-
-	//~ This works:
-	//db.Model(&userPointer).Update("fid", "20")
-
-  	
-	
-}
-
-/*	//& Below are some more official functions to implement 
-	what I have written up above a little better in terms of
-	the user pointer
-
+/*	//& The main database functions are written below
 	Basically, one function retrieves the user pointer & db, and the
 	rest of the functions will accept a user pointer & db to do their
 	necessary operations
@@ -147,13 +75,15 @@ func dataBaseTesting(username string, em string, uID string){
 	updateUserDatabase(userPointer, userDB)
 
 	printUserDatabase(userPointer)
+
+	//deleteRow(userPointer, 2, userDB)
 }
 
 //for accessing the user pointer
 func getUserPointer(username string, em string, uID string) (Users, *gorm.DB){
 	
 	//opening the test database
-	db, err := gorm.Open(sqlite.Open("test2.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("test3.db"), &gorm.Config{})
 	if err != nil {
 	  panic("failed to connect database")
 	}
@@ -202,20 +132,41 @@ func printUserDatabase(userPointer Users){
 	
 }
 
+//deleting one to see if primary keys work
+func deleteRow(userPointer Users, theID uint, db *gorm.DB){
+	db.Unscoped().Delete(&userPointer, theID)
+}
+
+
+//For testing the fortuneTimer
 func fortuneTimerTesting(userPointer Users){
+	submittedCheck(userPointer)
+}
+
+//will check if the date has changed and updates Submitted Accordingly
+func submittedCheck(userPointer Users){
 	var hasChanged bool = false
 	hasChanged = checkTime(userPointer)
 
 	log.Println()
 	log.Println("Has our time changed? ", hasChanged)
-	//if the time has changed, update userPointer
-
+	
+	//if the time has changed, update userPointer.
 	if (hasChanged){
 		userPointer.LastTime = updateTime(userPointer)
-		userPointer.Submitted = true
-	} else{
 		userPointer.Submitted = false
+	} else{
+		userPointer.Submitted = true
 	}
+}
+
+//Will update LastTime and Submitted, for when a fortune is submitted
+func fortuneSubmitted(userPointer Users, db *gorm.DB){
+	userPointer.LastTime = updateTime(userPointer)
+	userPointer.Submitted = false
+
+	db.Model(&userPointer).Update("submitted", userPointer.Submitted)
+	db.Model(&userPointer).Update("last_time", userPointer.LastTime)
 }
 
 
@@ -238,4 +189,74 @@ func fortuneTimerTesting(userPointer Users){
 
 	* Create
 	db.Create(&People{Username: user, Email: em, UserID: uID, Temp: 5})
+
+	* Below is any old functions we will ignore now and delete later
+	
 */
+
+// func accessDatabase(username string, em string, uID string) {
+// 	log.Println("Authorization successful!")
+// 	log.Println("Username: ", username)
+// 	log.Println("Email: ", em)
+// 	log.Println("UID: ", uID)
+
+// 	//opening the test database
+// 	db, err := gorm.Open(sqlite.Open("test2.db"), &gorm.Config{})
+// 	if err != nil {
+// 	  panic("failed to connect database")
+// 	}
+  
+// 	// Migrate the schema
+// 	db.AutoMigrate(&Users{})
+
+// 	//Initialize struct variable
+// 	var userPointer Users
+
+// 	//!For Testing purposes, I'm deleting the first entry each time we restart the program
+// 	//db.Unscoped().Delete(&userPointer, 1)
+// 	//db.Unscoped().Delete(&userPointer, 2)
+	
+// 	//Create
+// 	ourPerson := Users{Username: username, Email: em, UserID: uID, Fid: "", Submitted: false, LastTime: time.Date(2002, time.January, 1, 23, 0, 0, 0, time.UTC)}
+
+// 	//Check if we have this user in the database already, or if we need to make a new row
+// 	//~ I made log outputs for testing purposes, but after that is the commented out version of the same thing but less code
+// 	result := db.Find(&userPointer, "user_id = ?", uID)
+// 	if (result.RowsAffected <= 0){
+// 		db.Create(&ourPerson)
+// 		log.Println("Our person wasn't found, so we made a new entry to the database")
+// 	} else{
+// 		log.Println("Our person found!")
+// 	}
+
+// 	/*// ! This is the same thing as above but less code, replace once we're sure things are ok.
+// 	result := db.Find(&userPointer, "user_id = ?", uID)
+// 	if (result.RowsAffected <= 0){
+// 		db.Create(&ourPerson)
+// 	} 
+// 	*/
+
+// 	//Setting the pointer so it can retrieve the userID and also update the database
+// 	db.First(&userPointer, "user_id = ?", uID).Scan(&userPointer)
+
+// 	// ~ Testing fortuneTimer
+// 	var hasChanged bool = false
+// 	hasChanged = checkTime(userPointer)
+// 	log.Println("\nHas our time changed? ", hasChanged)
+
+// 	//Updating the database after any changes
+// 	db.Model(&userPointer).Update("submitted", userPointer.Submitted)
+// 	db.Model(&userPointer).Update("fid", userPointer.Fid)
+// 	db.Model(&userPointer).Update("last_time", userPointer.LastTime)
+
+// 	log.Println(userPointer.ID, " is my database ID")
+	
+// 	// ~ For Testing
+// 	// log.Println("Our temp is ", anotherPerson.Temp)
+// 	// log.Println(userPointer.UserID, " is my database ID")
+// 	// log.Println(userPointer.Email, " is my database email")
+
+// 	//~ This works:
+// 	//db.Model(&userPointer).Update("fid", "20")
+
+// }
