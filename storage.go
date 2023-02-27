76f835f265c0ff8e441de616ee214e6918b975a9
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2023-02-26 21:11:55 -0500

    Timing Updates
    Used package 'time' to check if the last time that a fortune has been done was at least a day before what the user has saved.
    Also created a new test database

--- storage.go
+++ storage.go
@@ -14,12 +14,20 @@
 	- Also mini note, although the People struct has variable names, the
 	actual column names in the database are different. I don't know why, 
 	just look at the table and not your variables.
+
+& TO DO
+	- Need a timer / date thing to see what to set the bool Submitted to	
+	- Need a way to add to the fortunes database
+	- Need a way to retrieve from the fortunes database 
+		- use random numbers 1 - length of database
+		- Need to make sure they don't receive their own fortunes
 */
 
 package main
 
 import (
 	"log"
+	"time"
 
 	"gorm.io/gorm"
   	"gorm.io/driver/sqlite"
@@ -34,7 +42,16 @@ type Users struct {
 
 	Fid string				//Stores the Ids of all the fortunes recieved, for history. It's in order.
 	Submitted bool			//If a fortune has been submitted by them today.
-	LastFortune string		//When the last fortune was submitted. Is used to find out if 24 hours has passed.
+	LastTime time.Time			//When the last fortune was submitted. Is used to find out if 24 hours has passed.
+
+  }
+
+  type Fortunes struct {
+	gorm.Model
+
+	Fid string				`gorm:"primaryKey;autoIncrement:false"`
+	Author string
+	Text string
 
   }
 
@@ -45,7 +62,7 @@ func accessDatabase(username string, em string, uID string) {
 	log.Println("UID: ", uID)
 
 	//opening the test database
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("test2.db"), &gorm.Config{})
 	if err != nil {
 	  panic("failed to connect database")
 	}
@@ -57,11 +74,11 @@ func accessDatabase(username string, em string, uID string) {
 	var userPointer Users
 
 	//!For Testing purposes, I'm deleting the first entry each time we restart the program
-	// db.Unscoped().Delete(&userPointer, 1)
-	// db.Unscoped().Delete(&userPointer, 2)
+	//db.Unscoped().Delete(&userPointer, 1)
+	//db.Unscoped().Delete(&userPointer, 2)
 	
 	//Create
-	ourPerson := Users{Username: username, Email: em, UserID: uID, Fid: "", Submitted: false, LastFortune: "I dunno"}
+	ourPerson := Users{Username: username, Email: em, UserID: uID, Fid: "", Submitted: false, LastTime: time.Date(2002, time.January, 1, 23, 0, 0, 0, time.UTC)}
 
 	//Check if we have this user in the database already, or if we need to make a new row
 	//~ I made log outputs for testing purposes, but after that is the commented out version of the same thing but less code
@@ -80,8 +97,19 @@ func accessDatabase(username string, em string, uID string) {
 	} 
 	*/
 
-	//Let's try to retrieve value Temp from current User, going to try scan
+	//Setting the pointer so it can retrieve the userID and also update the database
 	db.First(&userPointer, "user_id = ?", uID).Scan(&userPointer)
+
+	// ~ Testing fortuneTimer
+	var hasChanged bool = false
+	userPointer.LastTime, hasChanged = checkTime(userPointer)
+	log.Println("\nHas our time changed? ", hasChanged)
+
+	//Updating the database after any changes
+	db.Model(&userPointer).Update("submitted", userPointer.Submitted)
+	db.Model(&userPointer).Update("fid", userPointer.Fid)
+	db.Model(&userPointer).Update("last_time", userPointer.LastTime)
+
 	
 	// ~ For Testing
 	// log.Println("Our temp is ", anotherPerson.Temp)
