f4647f9b3043fb2cd3c6e25cba0e664d60c5f5b2
bencrbcp <bencrbcp@gmail.com>
2023-03-01 15:36:38 -0500

    new tutorial time

--- frontend/src/app/okta.config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    issuer: 'https://dev-21868273.okta.com/oauth2/default',
-    clientId: '0oa8jamvsyYwGf6AD5d7',
-    redirectUri: 'http://localhost:8080/implicit/callback',
-    scope: 'openid profile email'
-}
--- frontend/src/app/shared/models/post.ts
+++
@@ -1,7 +0,0 @@
-export interface IPost {
-  id?: number;
-  title: string;
-  description: string;
-  body: string;
-  user_email: string;
-}
