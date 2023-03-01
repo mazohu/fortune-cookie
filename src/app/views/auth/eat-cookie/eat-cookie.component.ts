fd2c43dbd7a74406573e6c603a0c98f668f54888
delicious-echidna <73537263+delicious-echidna@users.noreply.github.com>
2023-03-01 18:17:11 -0500

    Update eat-cookie.component.ts

--- src/app/views/auth/eat-cookie/eat-cookie.component.ts
+++ src/app/views/auth/eat-cookie/eat-cookie.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-eat-cookie',
   templateUrl: './eat-cookie.component.html',
   styleUrls: ['./eat-cookie.component.css']
 })
+
 export class EatCookieComponent implements OnInit {
 
-  constructor() { }
+  writeFortune = this.formBuilder.group({
+    Fortune:''
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+
+  ) {}
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    // Process checkout data here
+    console.warn('Your fortune has been submitted', this.writeFortune.value);
+    this.writeFortune.reset();
   }
 
 }
