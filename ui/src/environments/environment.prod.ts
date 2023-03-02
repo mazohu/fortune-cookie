5b8538acb3883bb04ac3eed6fd3780ab351403b3
bencrbcp <bencrbcp@gmail.com>
2023-03-01 19:08:52 -0500

    epic todo list with auth0 authentication completed

---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  gateway: '',
+  callback: 'http://localhost:3000/callback',
+  domain: 'dev-8sqvoimkafxur22g.us.auth0.com',
+  clientId: '3HGvqRgUJKsZQxJR2QxyyZ34c6j1xNny',
+  audience: 'https://fortune-api/'
+};
