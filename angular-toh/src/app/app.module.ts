3ba5dd5a89e7cb9458cd40dc98b49015eab398cb
delicious-echidna <73537263+delicious-echidna@users.noreply.github.com>
2023-04-17 13:01:07 -0400

    Angular tutorial
    Doing this to learn how to integrate the front and back-end since we are using a similar system to get each fortune

---
+++ angular-toh/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
