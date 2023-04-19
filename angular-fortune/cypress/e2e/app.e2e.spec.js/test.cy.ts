bc7f9341fd42f6747aec213c9e18142a79b87e75
ben <bencrbcp@gmail.com>
2023-04-19 19:34:16 -0400

    added cypress e2e testing

---
+++ angular-fortune/cypress/e2e/app.e2e.spec.js/test.cy.ts
@@ -0,0 +1,7 @@
+
+describe('Tour of Heroes App Testing', () => {
+    it(`Should have the title 'Fortune Cookie'`, () => {
+        cy.visit('localhost:4200')
+        cy.contains('Fortune Cookie');
+    });
+});
